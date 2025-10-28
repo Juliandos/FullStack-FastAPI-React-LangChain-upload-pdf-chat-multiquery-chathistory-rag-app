105c50b5dda5bd51b337358fb84f1a476698d84e
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-10-28 10:39:16 -0500

    Guia de puesta en marcha

--- v1-166-part5/app/server.py
+++ v1-166-part5/app/server.py
@@ -14,6 +14,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000"
+        "http://localhost:3001",
+        "http://localhost:3002"
     ],
     allow_credentials=True,
     allow_methods=["*"],
